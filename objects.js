608a58a521c9234fe9e245a74caaddd77f9bd81b
Joseph Parker <jparker010190@gmail.com>
2018-03-05 01:16:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,8 @@
 var playlist = {artistname: 'song titles'};
 function updatePlaylist (playlist, key, value){
-  return Object.assign({}, obj,{[key]:value})
+  return Object.assign({}, playlist, {[key]:value})
 }
-function removeFromPlaylist ()
+function removeFromPlaylist (playlist, artistname){
+  delete playlist.artistname;
+  return playlist
+}
