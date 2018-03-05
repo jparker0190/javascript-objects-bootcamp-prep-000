02edf0742e9a4021eafdf0e705df72d4f44ec038
Joseph Parker <jparker010190@gmail.com>
2018-03-05 01:00:20 +0000

    automatically backed up by learn

--- objects.js
+++
@@ -1,7 +0,0 @@
-var meals = {};
-var meals = new Object();
-var meals = {breakfast: "oatmeal"};
-var meals = new Object({breakfast: 'oatmeal'})
-var meals = {breakfast: 'eggs' breakfast:'bacon'}
-var meals = {breakfast:'avocado', lunch:'avocado', dinner:'avocado'}
-const firstMeal = 'breakfast'
