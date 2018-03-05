25150a855a6d71adaf2755ad7eb2167cb7acc71a
Joseph Parker <jparker010190@gmail.com>
2018-03-05 01:43:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ function updatePlaylist (playlist, key, value){
   return Object.assign({}, playlist, {[key]:value})
 }
 function removeFromPlaylist (playlist, artistName){
-  var joe = delete playlist['artistName']
-  return joe;
+  delete playlist.artistName
+  return playlist
 }
